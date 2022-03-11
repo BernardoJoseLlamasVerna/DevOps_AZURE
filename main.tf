eb66c06fe7146e38f13bef954da15dd2cdb21e8c
Bernardo Llamas Verna <bernardo.llamas@sngular.team>
2022-03-11 14:04:19 +0100

    subida archivos

---
+++ main.tf
@@ -0,0 +1,46 @@
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.1"
+    }
+  }
+}
+
+# crea un service principal y rellena los siguientes datos para autenticar
+provider "azurerm" {
+  features {}
+  subscription_id = "77c88c55-f1f1-487b-91fd-8d3122bf6a57"
+  client_id       = "66140cd9-607e-4d24-84db-92061d00ffde"
+  client_secret   = "2uMylDduNa9z.Kf2g2MTojv9BV9d~x8_1g"
+  tenant_id       = "899789dc-202f-44b4-8472-a6d40f9eb440"
+}
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/resource_group
+
+resource "azurerm_resource_group" "rg" {
+    name     =  "kubernetes_rg"
+    location = var.location
+
+    tags = {
+        environment = "CP2"
+    }
+
+}
+
+# Storage account
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account
+
+resource "azurerm_storage_account" "stAccount" {
+    name                     = "berukwestaccountcp2" 
+    resource_group_name      = azurerm_resource_group.rg.name
+    location                 = azurerm_resource_group.rg.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+
+    tags = {
+        environment = "CP2"
+    }
+}
